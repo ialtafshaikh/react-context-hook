59d29d2ccfed7a4835fcf0aa77e50acf731a93ac
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-18 15:03:10 +0530

    context hook - component created and linked

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext } from "react";
+import "./App.css";
+import ComponentC from "./components/ComponentC";
+
+// firstly, create a hook
+export const userContext = createContext();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Understanding React Hook - useContext</h1>
+        {/* secondly provide a provider to the component */}
+        <userContext.Provider>
+          <ComponentC />
+        </userContext.Provider>
       </header>
     </div>
   );
