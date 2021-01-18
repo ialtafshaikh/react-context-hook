96918fd748f730bd34cfbe5f747de1b3560eea10
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-18 15:14:30 +0530

    multiple context value using useContext API

--- src/components/ComponentE.jsx
+++ src/components/ComponentE.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import ComponentF from "./ComponentF";
+import { channelContext, userContext } from "../App";
 
 export default function ComponentE() {
-  return <ComponentF />;
+  const user = useContext(userContext);
+  const channel = useContext(channelContext);
+
+  return (
+    <div>
+      {" "}
+      User is {user} and channel is {channel}
+    </div>
+  );
 }
